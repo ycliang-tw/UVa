0e2915e940a2b01e4a4c77570a1e52353a21f402
ycliang-tw <ycliang@cs.nctu.edu.tw>
2019-04-17 14:07:54 +0800

    Support dry-run arg in move_file_to_dir scripts and add templates.

--- move_file_to_dir.sh
+++ move_file_to_dir.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
+#######################################################################
+# The difference between "if []; then" and "[] &&/|| instruction" is ##
+# that the execution result($?) of the former will always be true    ##
+#######################################################################
 
-main()
+
+DRY_RUN=0
+
+function usage()
 {
+	echo "usage: ./move_file_to_dir <option>"
+	echo "    -d, --dry-run        Do not really move the files, only shows what actions will be done."
+	echo "    -h, --help           Show this message. "
+	echo "default action will move all the files to their corresponding directory"
+}
+
+function move_file_to_dir()
+{
+	
 	files=`ls | grep cpp`
 	for file in $files
 		do 
-			ofile=$file
+			origin_file=$file
 			
-			file=`echo $file | cut -d"." -f1`
-			len=${#file}
-			file=${file:0:(($len-2))}
-			dir=UVa$file
+			tmp=`echo $file | cut -d"." -f1`
+			len=${#tmp}
+			ID=${tmp:0:(($len-2))}
+			dir=UVa$ID
 			
 			[ ! -d $dir ] && mkdir $dir
-			echo -e "mv $ofile $dir"
-			mv $ofile $dir
+			echo -e "mv $origin_file $dir"
+			if [ "DRY_RUN" = "1" ]; then
+				mv $origin_file $dir
+			fi
 		done
-
 }
 
-main 
+function main()
+{
+	until [ -z "$1" ]
+	do
+		case "$1" in
 
+			-h | --help)
+				usage
+				exit 0;;
+
+			-d | --dry-run)
+				DRY_RUN=1
+				break;;
+
+			*)
+				usage
+				exit 1;;
+		esac
+	done
+
+	move_file_to_dir
+}
+
+main "$@"
